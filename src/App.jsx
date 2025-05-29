2c842acf6ec5522ff629b70bfad39095e63fd7ab
Emmanuel Kyeremeh <josephkyeremeh53@gmail.com>
2025-05-29 11:00:17 +0000

    implemented 3d rendering

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import IfcViewer from "./IfcViewer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🏗️ IFC Viewer</h1>
+      <IfcViewer />
+    </div>
+  );
+}
+
+export default App;
