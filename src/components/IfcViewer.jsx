8b0e43b334b62f31d08612c88b62f40581e81ae0
Emmanuel Kyeremeh <josephkyeremeh53@gmail.com>
2025-05-30 15:44:27 +0000

    path bug fix

--- src/components/IfcViewer.jsx
+++ src/components/IfcViewer.jsx
@@ -17,7 +17,7 @@ import {
 } from "@heroicons/react/24/outline";
 import FloorPlanControls from "./FloorPlanControls";
 import ModelInfo from "./ModelInfo";
-import "/src/styles/IfcViewer.css";
+import "../styles/IfcViewer.css";
 
 const IfcViewer = ({ ifcFile }) => {
   const sceneDataRef = useRef(null);
